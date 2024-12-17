b389c718068e3f906b3009eee651dec619e8e6fa
Michal Dmoch <01142163@pw.edu.pl>
2024-12-17 13:57:35 +0100

    Add project files.

---
+++ SANDBOX-ClubMembershipApplication/Data/ClubMembershipDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SANDBOX_ClubMembershipApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SANDBOX_ClubMembershipApplication.Data
+{
+    public class ClubMembershipDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={AppDomain.CurrentDomain.BaseDirectory}ClubMembershipDb.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
