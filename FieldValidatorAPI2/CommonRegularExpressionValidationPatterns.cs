c72f4de1d61361306b12fd4469d3cbd6148f1d92
Michal Dmoch <m.dmoch.biz@gmail.com>
2024-12-19 12:00:36 +0100

    Changed the type of FieldValidatorAPI project and added other field validators

---
+++ FieldValidatorAPI2/CommonRegularExpressionValidationPatterns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldValidatorAPI2
+{
+    public static class CommonRegularExpressionValidationPatterns
+    {
+        public const string Email_Address_RegEx_Pattern = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+
+        public const string PL_PhoneNumber_RegEx_Pattern = @"^(\+48\s?)?(\d{3}[-\s]?\d{3}[-\s]?\d{3})$";
+
+        public const string PL_Post_Code_RegEx_Pattern = @"^\d{2}-\d{3}$";
+
+        public const string Strong_Password_RegEx_Pattern = @"(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$";
+
+    }
+}
---
+++ SANDBOX-ClubMembershipApplication/FieldValidators/UserRegistrationValidator.cs
@@ -0,0 +1,42 @@
+﻿using FieldValidatorAPI2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SANDBOX_ClubMembershipApplication.FieldValidators
+{
+    public class UserRegistrationValidator
+    {
+        const int FirstName_Min_Llength = 2;
+        const int FirstName_Max_Llength = 100;
+        const int LastName_Min_Llength = 2;
+        const int LastName_Max_Llength = 100;
+
+        delegate bool EmailExistsDel(string emailAddress);
+
+        FieldValidatorDel _fieldValidatorDel = null;
+
+        RequiredValidDel _requiredValidDel = null;
+        StringLengthValidDel _stringLenthValidDel = null;
+        DateValidDel _dateValidDel = null;
+        PatternMatchValidDel _patternMatchValidDel = null;
+        CompareFieldsValidDel _compareFieldsValidDel = null;
+
+        EmailExistsDel ExistsDel = null;
+
+        string[] _fieldArray = null;
+
+        public string[] FieldArray
+        {
+            get 
+            {
+                if (_fieldArray == null)
+                    _fieldArray = new string[Enum.GetValues(typeof(FieldConstants.UserRegistrationField)).Length];
+                return _fieldArray; 
+            }
+        }
+        public FieldValidatorDel ValidatorDel => _fieldValidatorDel;
+    }
+}
